31c4576301a18b2b20504dbb08ae9c7c2a60cd62
Elizaveta Tishkovets <tiishka2@gmail.com>
2021-03-18 09:46:56 +0300

    Обноавление БД, добвлена связь курьер-заказ

--- store/models/courier.py
+++ store/models/courier.py
@@ -1,13 +1,16 @@
 from store import db
+from store.models.order import Order
 
 
 class Courier(db.Model):
-    courier_id = db.Column(db.Integer, primary_key=True)
+    courier_id = db.Column(db.Integer, primary_key=True, unique=True)
     courier_type = db.Column(db.String(30))
     regions = db.Column(db.ARRAY(db.Integer))
     working_hours = db.Column(db.ARRAY(db.String(30)))
     rating = db.Column(db.Integer)
     earnings = db.Column(db.Integer)
+
+    orders = db.relationship(Order, backref=db.backref('courier'))
 
     def from_dict(self, data):
         for field in ['courier_id', 'courier_type', 'regions', 'working_hours']:
--- store/models/order.py
+++ store/models/order.py
@@ -8,6 +8,12 @@ class Order(db.Model, JsonMixin):
     region = db.Column(db.Integer)
     delivery_hours = db.Column(db.ARRAY(db.String))
 
+    courier_id = db.Column(db.Integer, db.ForeignKey('courier.courier_id'))
+
+    is_complete = db.Column(db.Boolean, default=False)
+    is_assign = db.Column(db.Boolean, default=False)
+
+
     def from_dict(self, data):
         for field in ['order_id', 'weight', 'regions', 'delivery_hours']:
             if field in data:
